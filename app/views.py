3b69b953baba5cd43184f53b12aa67c8eb2b540a
Dusty Argyle <dustymargyle@gmail.com>
2018-02-17 17:11:52 -0700

    Adding jupyter notebook with how to in it. Add the form to it

--- app/views.py
+++ app/views.py
@@ -11,20 +11,27 @@ def test(request):
     return HttpResponse('My second view!')
 
 def profile(request):
-    json_list = []
     parsed_data = []
-    user_data = {}
-    req = requests.get('https://api.github.com/users/dustycodes')
-    json_list.append(json.loads(req.text))
-    for data in json_list:
-        user_data['name'] = data['name']
-        user_data['blog'] = data['blog']
-        user_data['email'] = data['email']
-        user_data['public_gists'] = data['public_gists']
-        user_data['public_repos'] = data['public_repos']
-        user_data['avatar_url'] = data['avatar_url']
-        user_data['followers'] = data['followers']
-        user_data['following'] = data['following']
+    if request.method == 'POST':
+        user_name = request.POST.get('user')
+        json_list = []
+        user_data = {}
 
-    parsed_data.append(user_data)
+        req = requests.get('https://api.github.com/users/' + user_name)
+        if req.status_code is 200:
+            json_list.append(json.loads(req.text))
+            for data in json_list:
+                user_data['name'] = data['name']
+                user_data['blog'] = data['blog']
+                user_data['email'] = data['email']
+                user_data['public_gists'] = data['public_gists']
+                user_data['public_repos'] = data['public_repos']
+                user_data['avatar_url'] = data['avatar_url']
+                user_data['followers'] = data['followers']
+                user_data['following'] = data['following']
+        else:
+            user_data['name'] = "User does not exist"
+
+        parsed_data.append(user_data)
+
     return render(request, 'app/profile.html', {'data': parsed_data})
